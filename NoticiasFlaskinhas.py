802dd53481fd4effb34a4b4c5fbc75c22ec09577
Pedro Mendonça <pedro.mendonca@edglobo.com.br>
2023-09-27 10:54:37 -0300

    Cors

--- NoticiasFlaskinhas.py
+++ NoticiasFlaskinhas.py
@@ -1,10 +1,12 @@
 import flask
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask("Noticias Flaskinhas")
 from Controllers.NewsController import *
 
 def run_app():
+    CORS(app)
     app.run(debug=True)
 
 if __name__ == '__main__':
